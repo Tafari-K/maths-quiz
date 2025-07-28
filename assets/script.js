2179e86ae9491137b303fe8f183516de720488c1
Tafari-K <kirton.tafari@gmail.com>
2025-07-28 18:34:37 +0100

    Add: include division in questions

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function generateQuestion() {
         const num1 = Math.floor(Math.random() *10) + 1;
         const num2 = Math.floor(Math.random() *10) + 1;
-        const operators = ['+','-','*'];
+        const operators = ['+','-','*','/'];
         const operator = operators[Math.floor(Math.random()* operators.length)];
 
 //creating question
