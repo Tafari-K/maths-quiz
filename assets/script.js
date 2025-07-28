853c8db4c7f8eb6b319f09ef1d4fcf5d642d3489
Tafari-K <kirton.tafari@gmail.com>
2025-07-28 16:51:11 +0100

    Add: id tag and start game logic on script.js

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+document.addEventListener
+
+//Game settings
+    const totalQuestions = 10;
+    let currentQuestion = 0
+    let score = 0
+
+//Elements from HTML page
+    const questionElement = document.getElementById('question');
+    const answerInput = document.getElementById('answer');
+    const submitButton = document.getElementById('submit-btn');
+    const feedbackElement = document.getElementById('feedback');
+    const scoreElement = document.getElementById('score');
+
