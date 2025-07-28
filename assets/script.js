0664c25abc11f41edef0c8cce6ab3e8e6dfbb695
Tafari-K <kirton.tafari@gmail.com>
2025-07-28 21:24:21 +0100

    Refactor: Correct spelling and change logic formattion

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
 
 //Game settings
     const totalQuestions = 10;
@@ -6,11 +6,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let score = 0
 
 //Elements from HTML page
-    const questionElement = document.getElementById('question');
-    const answerInput = document.getElementById('answer');
-    const submitButton = document.getElementById('submit-btn');
-    const feedbackElement = document.getElementById('feedback');
-    const scoreElement = document.getElementById('score');
+    const questionElement = document.getElementById("question");
+    const answerInput = document.getElementById("answer");
+    const submitButton = document.getElementById("submit-btn");
+    const feedbackElement = document.getElementById("feedback");
+    const scoreElement = document.getElementById("score");
 
 //Store current correct answer
     let correctAnswer = 0;
@@ -19,19 +19,25 @@ document.addEventListener('DOMContentLoaded', function () {
     function generateQuestion() {
         const num1 = Math.floor(Math.random() *10) + 1;
         const num2 = Math.floor(Math.random() *10) + 1;
-        const operators = ['+','-','*','/'];
+        const operators = ["+","-","*","/"];
         const operator = operators[Math.floor(Math.random()* operators.length)];
 
 //creating question
-        let questionText = '${num1} ${operator} ${num2}';
+        let questionText = "${num1} ${operator} ${num2}";
 
 //checking the answer 
-        correctAnswer = eval(questionText);
+        switch (operator) {
+            case "+": correctAnswer = num1 + num2; break;
+            case "-": correctAnswer = num1 - num2; break;
+            case "*": correctAnswer = num1 * num2; break;
+            case "/": correctAnswer = Math.round(num1 / num2); break;
+        }
+        
 
 // display the question
-        questionElement.textContent = 'Question ${currentQuestion + 1}: ${questionText}';
-        answerInput.value = '';
-        feedbackElement.textContent = '';
+        questionElement.textContent = "Question ${currentQuestion + 1}: ${questionText}";
+        answerInput.value = ";
+        feedbackElement.textContent = ";
     }
 
 //checking answer is correct
@@ -40,20 +46,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //input validation to avoid letters
         if(isNaN(userAnswer)){
-            feedbackElement.tentContent = 'Please enter a number!';
+            feedbackElement.textContent = "Please enter a number!";
             return;
         }
 
 //check answer and update score
         if(userAnswer === correctAnswer) {
             score++;
-            feedbackElement.textContent = ' ✅ Correct!';
+            feedbackElement.textContent = " ✅ Yes Correct!";
         } else {
-            feedbackElement.textContent = ' ❌ Sorry that was inccorect the answer was ${correctAnswer}';
+            feedbackElement.textContent = " ❌ Sorry that was incorrect the answer, was ${correctAnswer}";
         }
             
 //Update score display
-    scoreElement.textContent = 'Score: ${score}';
+    scoreElement.textContent = "Score: ${score}";
 
 //Next question or end game
     currentQuestion++;
@@ -66,11 +72,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //End of quiz results and scores
 function showFinalScore() {
-    localStorage.setItem('mathsQuizScore', score);
-    window.location.href = 'result.html';
+    localStorage.setItem("mathsQuizScore", score);
+    window.location.href = "result.html";
 }
 
-submitButton.addEventListener('click', checkAnswer);
+submitButton.addEventListener("click", checkAnswer);
 
 generateQuestion();
 
